3f531c939cdc7dca130c97ab1a783d281565624a
PRINCEKK121 <prince.karikari@amalitech.org>
2021-04-16 00:32:26 +0000

    Selecting elements in the DOM

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 window.addEventListener('load', function () {
+  // Elements
   const searchBtn = document.querySelector('#searchBtn');
-  
+  const searchResults = document.querySelector('#results');
+  const searchTerm = document.querySelector('#hobbies');
+  const gender = document.querySelector('#gender');
+
   searchBtn.addEventListener('click', function() {
-    console.log('The search button was clicked');
+    console.log(searchTerm.value);
+    console.log(gender[gender.selectedIndex].value);
   });
 });
