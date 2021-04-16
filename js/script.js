db3adfc20a1064902aec0695919a28a86f184ec1
PRINCEKK121 <prince.karikari@amalitech.org>
2021-04-16 02:10:10 +0000

    Implementing search results "gender and hobby"

--- js/script.js
+++ js/script.js
@@ -2,29 +2,36 @@ const users = [
   { name: 'Prince', gender: 'M', hobby: 'coding', avatar: 'avatar0.png' },
   { name: 'Peggy', gender: 'F', hobby: 'cooking', avatar: 'avatar1.png' },
   { name: 'Seth', gender: 'M', hobby: 'gaming', avatar: 'avatar2.png' },
-  { name: 'Endeavor', gender: 'M', hobby: 'flirting', avatar: 'avatar3.png' },
+  { name: 'Endeavor', gender: 'M', hobby: 'gaming', avatar: 'avatar3.png' },
   { name: 'Luffy', gender: 'M', hobby: 'joking', avatar: 'avatar4.png' },
   { name: 'Ama', gender: 'F', hobby: 'selling', avatar: 'avatar5.png' },
-  { name: 'Belinda', gender: 'F', hobby: 'sleeping', avatar: 'avatar6.png' },
-  { name: 'Mike', gender: 'M', hobby: 'fashion', avatar: 'avatar7.png' },
-  { name: 'Rose', gender: 'F', hobby: 'studying', avatar: 'avatar8.png' },
-  { name: 'Anita', gender: 'F', hobby: 'pets', avatar: 'avatar9.png' },
+  { name: 'Belinda', gender: 'F', hobby: 'pets', avatar: 'avatar6.png' },
+  { name: 'Mike', gender: 'M', hobby: 'coding', avatar: 'avatar7.png' },
+  { name: 'Rose', gender: 'F', hobby: 'pets', avatar: 'avatar8.png' },
+  { name: 'Anita', gender: 'F', hobby: 'cooking', avatar: 'avatar9.png' },
 ];
 
 window.addEventListener('load', function () {
-  // Elements
+  // elements
   const searchBtn = document.querySelector('#searchBtn');
   const results = document.querySelector('#results');
-  const searchTerm = document.querySelector('#hobbies');
   const gender = document.querySelector('#gender');
 
-  // Functions
-  const generateHTML = function (users) {
+  // functions
+  const generateHTML = (users) => {
+    // grabbing all the user input
+    const selectedGender = gender[gender.selectedIndex].value;
+    const searchTerm = document.querySelector('#hobbies').value;
+
     let resultsHTML = '';
 
-    users.forEach(
-      (user, index) =>
-        (resultsHTML += `
+    users.forEach((user, index) => {
+      const userChoice =
+        (selectedGender === 'A' || selectedGender === user.gender) &&
+        (searchTerm === '' || searchTerm === user.hobby);
+
+      if (userChoice) {
+        resultsHTML += `
           <div class="person-row">
             <img src="images/avatar${index}.png" alt='${user.name}'>
             <div class="person-info">
@@ -32,19 +39,17 @@ window.addEventListener('load', function () {
               <div>${user.hobby}</div>
             </div>
             <button>Add as a friend</button>
-          </div>`)
-    );
+          </div>`;
+      }
+    });
 
-    results.innerHTML = resultsHTML;
+    return resultsHTML;
   };
 
   // generating the template
   generateHTML(users);
 
   searchBtn.addEventListener('click', function () {
-    console.log(searchTerm.value);
-    console.log(gender[gender.selectedIndex].value);
-
-    searchResults.innerHTML = resultsHTML;
+    results.innerHTML = generateHTML(users);
   });
 });
