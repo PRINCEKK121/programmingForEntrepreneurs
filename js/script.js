fdf1f1cefa5dc0ddaccc6e71cef5382de71b694a
PRINCEKK121 <prince.karikari@amalitech.org>
2021-04-16 01:01:22 +0000

    Adding images and json user data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const users = [
+  { name: 'Prince', gender: 'M', hobby: 'coding', avatar: 'avatar1.png' },
+  { name: 'Peggy', gender: 'F', hobby: 'cooking', avatar: 'avatar2.png' },
+  { name: 'Seth', gender: 'M', hobby: 'gaming', avatar: 'avatar3.png'},
+  { name: 'Endeavor', gender: 'M', hobby: 'hero', avatar: 'avatar4.png' },
+  {name: 'Luffy', gender: 'M', hobby: 'joking', avatar: 'avatar5.png' },
+  { name: 'Ama', gender: 'F', hobby: 'selling', avatar: 'avatar6.png' },
+  { name: 'Belinda', gender: 'F', hobby: 'sleeping', avatar: 'avatar7.png' },
+  { name: 'Mike', gender: 'M', hobby: 'fashion', avatar: 'avatar8.png' },
+  { name: 'Rose', gender: 'F', hobby: 'studying', avatar: 'avatar9.png' },
+  { name: 'Anita', gender: 'F', hobby: 'pets', avatar: 'avatar10.png' }
+];
+
 window.addEventListener('load', function () {
   // Elements
   const searchBtn = document.querySelector('#searchBtn');
