78b2e24e077d0d8456553a74be0bec12ccb15a4a
PRINCEKK121 <prince.karikari@amalitech.org>
2021-04-16 02:34:51 +0000

    Adding and implementing age filtering

--- js/script.js
+++ js/script.js
@@ -1,14 +1,68 @@
 const users = [
-  { name: 'Prince', gender: 'M', hobby: 'coding', avatar: 'avatar0.png' },
-  { name: 'Peggy', gender: 'F', hobby: 'cooking', avatar: 'avatar1.png' },
-  { name: 'Seth', gender: 'M', hobby: 'gaming', avatar: 'avatar2.png' },
-  { name: 'Endeavor', gender: 'M', hobby: 'gaming', avatar: 'avatar3.png' },
-  { name: 'Luffy', gender: 'M', hobby: 'joking', avatar: 'avatar4.png' },
-  { name: 'Ama', gender: 'F', hobby: 'selling', avatar: 'avatar5.png' },
-  { name: 'Belinda', gender: 'F', hobby: 'pets', avatar: 'avatar6.png' },
-  { name: 'Mike', gender: 'M', hobby: 'coding', avatar: 'avatar7.png' },
-  { name: 'Rose', gender: 'F', hobby: 'pets', avatar: 'avatar8.png' },
-  { name: 'Anita', gender: 'F', hobby: 'cooking', avatar: 'avatar9.png' },
+  {
+    name: 'Prince',
+    age: 28,
+    gender: 'M',
+    hobby: 'coding',
+    avatar: 'avatar0.png',
+  },
+  {
+    name: 'Peggy',
+    age: 27,
+    gender: 'F',
+    hobby: 'cooking',
+    avatar: 'avatar1.png',
+  },
+  {
+    name: 'Seth',
+    age: 30,
+    gender: 'M',
+    hobby: 'gaming',
+    avatar: 'avatar2.png',
+  },
+  {
+    name: 'Endeavor',
+    age: 42,
+    gender: 'M',
+    hobby: 'gaming',
+    avatar: 'avatar3.png',
+  },
+  {
+    name: 'Luffy',
+    gender: 'M',
+    age: 19,
+    hobby: 'joking',
+    avatar: 'avatar4.png',
+  },
+  {
+    name: 'Ama',
+    gender: 'F',
+    age: 23,
+    hobby: 'selling',
+    avatar: 'avatar5.png',
+  },
+  {
+    name: 'Belinda',
+    gender: 'F',
+    age: 19,
+    hobby: 'pets',
+    avatar: 'avatar6.png',
+  },
+  {
+    name: 'Mike',
+    gender: 'M',
+    age: 19,
+    hobby: 'coding',
+    avatar: 'avatar7.png',
+  },
+  { name: 'Rose', gender: 'F', age: 22, hobby: 'pets', avatar: 'avatar8.png' },
+  {
+    name: 'Anita',
+    gender: 'F',
+    age: 24,
+    hobby: 'cooking',
+    avatar: 'avatar9.png',
+  },
 ];
 
 window.addEventListener('load', function () {
@@ -22,13 +76,17 @@ window.addEventListener('load', function () {
     // grabbing all the user input
     const selectedGender = gender[gender.selectedIndex].value;
     const searchTerm = document.querySelector('#hobbies').value;
+    const minAge = document.querySelector('#min-age').value;
+    const maxAge = document.querySelector('#max-age').value;
 
     let resultsHTML = '';
 
     users.forEach((user, index) => {
       const userChoice =
         (selectedGender === 'A' || selectedGender === user.gender) &&
-        (searchTerm === '' || searchTerm === user.hobby);
+        (searchTerm === '' || searchTerm === user.hobby) &&
+        user.age >= minAge &&
+        user.age <= maxAge;
 
       if (userChoice) {
         resultsHTML += `
